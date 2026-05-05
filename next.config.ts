f9d3f98c496427f52005afa0af081ba7253aa11b
setya433 <adisetya278@gmail.com>
2026-05-05 18:12:33 +0700

    update: update next configuration

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
+        // protocol: "http",
+        // hostname: "localhost",
+        // port: "1337",
+        // pathname: "/uploads/**",
+        protocol: "https",
+        hostname: "nama-project.strapiapp.com",
         pathname: "/uploads/**",
       },
     ],
